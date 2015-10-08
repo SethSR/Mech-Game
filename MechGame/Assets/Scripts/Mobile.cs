3d41710815db5fa587453cfda4035c85d4b29f7c
SethSR <srenshaw1@gmail.com>
2015-10-08 02:42:14 -0500

    Moved action decider code into a separate class

--- MechGame/Assets/Scripts/Mobile.cs
+++ MechGame/Assets/Scripts/Mobile.cs
@@ -5,21 +5,20 @@ using System.Linq;
 public class Mobile : MonoBehaviour {
 	public float maxSpeed;
 	public float maxForce;
-	public List<ActionDecider> actionDeciders;
 
 	[HideInInspector] public Vector3 velocity = Vector3.forward;
 
 	// Update is called once per frame
 	void Update () {
 		var steering_force = Vector3.zero;
-		foreach (var ad in actionDeciders) {
-			var force   = ad.Behavior.Force;
-			var utility = ad.considerations.Select(c => c.Utility).Aggregate((a,b) => a + b);
-			Debug.Log("Force: " + force + ", Utility: " + utility);
-			DebugExtension.DebugArrow(transform.position, force, Color.cyan);
-			steering_force += force;
-		}
-		steering_force = Vector3.ClampMagnitude(steering_force, maxForce);
+		// foreach (var ad in actionDeciders) {
+		// 	var force   = ad.Behavior.Force;
+		// 	var utility = ad.considerations.Select(c => c.Utility).Aggregate((a,b) => a + b);
+		// 	Debug.Log("Force: " + force + ", Utility: " + utility);
+		// 	DebugExtension.DebugArrow(transform.position, force, Color.cyan);
+		// 	steering_force += force;
+		// }
+		// steering_force = Vector3.ClampMagnitude(steering_force, maxForce);
 		DebugExtension.DebugArrow(transform.position, steering_force, Color.black);
 		var acceleration = steering_force / GetComponent<Rigidbody>().mass;
 		velocity += acceleration * Time.deltaTime;
