8fcab5eb640c7326d770377042116d2e587f75e5
SethSR <srenshaw1@gmail.com>
2015-10-08 02:39:08 -0500

    Added some stuff for testing ideas

---
+++ MechGame/Assets/Scripts/TestStuff/ListTester.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ListTester : MonoBehaviour {
+	public int        [] integers;
+	public Vector3    [] vectors;
+	public ColorPoint [] colorPoints;
+	public Transform  [] objects;
+	public int           notAList;
+}
