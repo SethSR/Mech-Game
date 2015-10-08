b4a16373c5dcce2d0e640e594e1ab6d33da5c0a2
SethSR <srenshaw1@gmail.com>
2015-10-08 15:07:25 -0500

    steering force is calculated elsewhere

--- MechGame/Assets/Scripts/Mobile.cs
+++ MechGame/Assets/Scripts/Mobile.cs
@@ -10,15 +10,8 @@ public class Mobile : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		var steering_force = Vector3.zero;
-		// foreach (var ad in actionDeciders) {
-		// 	var force   = ad.Behavior.Force;
-		// 	var utility = ad.considerations.Select(c => c.Utility).Aggregate((a,b) => a + b);
-		// 	Debug.Log("Force: " + force + ", Utility: " + utility);
-		// 	DebugExtension.DebugArrow(transform.position, force, Color.cyan);
-		// 	steering_force += force;
-		// }
-		// steering_force = Vector3.ClampMagnitude(steering_force, maxForce);
+		var steering_force = GetComponent<Reasoner>().Action;
+		steering_force = Vector3.ClampMagnitude(steering_force, maxForce);
 		DebugExtension.DebugArrow(transform.position, steering_force, Color.black);
 		var acceleration = steering_force / GetComponent<Rigidbody>().mass;
 		velocity += acceleration * Time.deltaTime;
