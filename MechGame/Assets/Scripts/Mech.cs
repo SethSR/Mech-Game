74c55c2533e242c6149226362e38ff68815e1d51
SethSR <srenshaw1@gmail.com>
2015-10-09 03:56:48 -0500

    Added some properties, they may stick...

---
+++ MechGame/Assets/Scripts/Mech.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mech : MonoBehaviour {
+	public float CurrentHealth { get; set; }
+	public float TotalHealth { get; set; }
+
+	public int CurrentTeamSize { get; set; }
+	public int TotalTeamSize { get; set; }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
