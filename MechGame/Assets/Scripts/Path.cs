0aa1d1fcbad98bc3e97e94a9863a460caca92d05
SethSR <srenshaw1@gmail.com>
2015-10-09 03:57:05 -0500

    Added a path class. It may stick...

---
+++ MechGame/Assets/Scripts/Path.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Path : MonoBehaviour {
+	public Vector3 CurrentWaypoint { get; set; }
+	public bool Finished { get; set; }
+
+	public Vector3 SetNextWaypoint() { return Vector3.zero; }
+}
