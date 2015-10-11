769c61d70185e7a6bf7881d25c5282d6c40c489e
SethSR <srenshaw1@gmail.com>
2015-10-10 22:46:19 -0500

    enactDecision now calculates utility well

--- MechGame/Assets/Scripts/Reasoner/Reasoner.cs
+++ MechGame/Assets/Scripts/Reasoner/Reasoner.cs
@@ -7,42 +7,39 @@ using System.Linq;
 public class Reasoner : BetterBehaviour {
 	public List<ActionDecider> actionDeciders;
 
-	Mobile mobile;
-
-	void Awake() {
-		mobile = GetComponent<Mobile>();
-	}
-
 	void Start() {
-		foreach (var ad in actionDeciders) {
-			ad.behavior.vehicle = mobile;
-			foreach (var con in ad.considerations) {
-				con.transform = transform;
-			}
-		}
+		actionDeciders.ForEach(ad =>
+			ad.considerations.ForEach(con =>
+				con.transform = transform));
+		// foreach (var ad in actionDeciders) {
+		// 	foreach (var con in ad.considerations) {
+		// 		con.transform = transform;
+		// 	}
+		// }
 	}
 
-	public Vector3 Action {
-		get {
-			var action = Vector3.zero;
-			var best_utility = 0f;
-			foreach (var ad in actionDeciders) {
-				if (ad.enable) {
-					var utility = ad.considerations.Select(c => c.Utility).Aggregate((a,b) => a * b);
-					utility = compensatedScore(utility, ad.considerations.Count);
-					Debug.Log(ad.actionName + ", utility: " + utility);
-					action       = best_utility < utility ? ad.behavior.Force : action;
-					best_utility = best_utility < utility ? utility : best_utility;
-				}
+	public void enactDecision() {
+		Decision decision = null;
+		var best_utility = 0f;
+		foreach (var ad in actionDeciders) {
+			if (ad.enable) {
+				var utility  = compensatedScore(
+					ad.considerations.Select(c => c.Utility).Aggregate((a,b) => a * b),
+					ad.considerations.Count);
+				Debug.Log(ad.actionName + ", utility: " + utility);
+				utility         *= ad.currentAction ? ad.commitmentBonus : 1;
+				decision         = best_utility < utility ? ad.decision : decision;
+				best_utility     = best_utility < utility ? utility : best_utility;
+				ad.currentAction = false;
 			}
-			return action;
 		}
+		actionDeciders.Find(ad => ad.decision == decision).currentAction = true;
+		decision.enact();
 	}
 
 	static float compensatedScore(float score, float numConsiderations) {
-		var modificationFactor      = 1 - (1 / numConsiderations);
-		var makeUpValue             = (1 - score) * modificationFactor;
-		var finalConsiderationScore = score + (makeUpValue * score);
-		return finalConsiderationScore;
+		var modFactor   = 1 - (1 / numConsiderations);
+		var makeUpValue = (1 - score) * modFactor;
+		return score + (makeUpValue * score);
 	}
 }
