68f806454d587081b08ea0f3bbd58a9da053606c
SethSR <srenshaw1@gmail.com>
2015-10-10 22:47:09 -0500

    Added several variables

--- MechGame/Assets/Scripts/Reasoner/ActionDecider.cs
+++ MechGame/Assets/Scripts/Reasoner/ActionDecider.cs
@@ -1,9 +1,12 @@
+using Vexe.Runtime.Types;
 using UnityEngine;
 using System.Collections.Generic;
 
 public class ActionDecider {
-  public bool                enable = true;
-  public string              actionName;
-  public SteeringBehavior    behavior;
-  public List<Consideration> considerations;
+	                  public bool                enable = true;
+	                  public string              actionName;
+	[fMin(1)]         public float               commitmentBonus;
+	                  public Decision            decision;
+	                  public List<Consideration> considerations;
+	[HideInInspector] public bool                currentAction;
 }
