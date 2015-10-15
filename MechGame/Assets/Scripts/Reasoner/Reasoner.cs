3d5e5e004c7467c8e96c40642e9fad47209277a4
SethSR <srenshaw1@gmail.com>
2015-10-15 00:52:05 -0500

    beginning to rework this

--- MechGame/Assets/Scripts/Reasoner/Reasoner.cs
+++ MechGame/Assets/Scripts/Reasoner/Reasoner.cs
@@ -6,34 +6,105 @@ using System.Linq;
 [RequireComponent(typeof(Mobile))]
 [RequireComponent(typeof(Mech))]
 public class Reasoner : BetterBehaviour {
-	public List<ActionDecider> actionDeciders;
+	List<Action>     actions;
+	List<Mech>       enemies;
+	List<GameObject> others;
 
-	void Start() {
-		actionDeciders.ForEach(ad => ad.initialize(GetComponent<Mech>(), transform));
-	}
+	public List<Action> actionDeciders = new List<Action>(5);
 
-	public void enactDecision() {
-		Decision decision = null;
+	Action currentAction = null;
+
+	public void enactDecision() { //TODO: need someway to use one action on multiple targets
+		Action chosen = null;
 		var best_utility = 0f;
 		foreach (var ad in actionDeciders) {
-			if (ad.enable) {
-				var utility  = compensatedScore(
-					ad.considerations.Select(c => c.Utility).Aggregate((a,b) => a * b),
-					ad.considerations.Count);
-				Debug.Log(ad.actionName + ", utility: " + utility);
-				utility         *= ad.currentAction ? ad.commitmentBonus : 1;
-				decision         = best_utility < utility ? ad.decision : decision;
-				best_utility     = best_utility < utility ? utility : best_utility;
-				ad.currentAction = false;
-			}
+			var utility = compensatedScore(ad.considerations.Select(c => c.Utility));
+			Debug.Log(ad.actionType + ", utility: " + utility);
+			utility     *= ad == currentAction ? ad.commitmentBonus : 1;
+			chosen       = best_utility < utility ? ad : chosen;
+			best_utility = best_utility < utility ? utility : best_utility;
 		}
-		actionDeciders.Find(ad => ad.decision == decision).currentAction = true;
-		decision.enact();
+		chosen.enact();
 	}
 
-	static float compensatedScore(float score, float numConsiderations) {
-		var modFactor   = 1 - (1 / numConsiderations);
+	static float compensatedScore(IEnumerable<float> scores) {
+		var score       = scores.Aggregate((a,b) => a * b);
+		var modFactor   = 1 - (1 / scores.Count());
 		var makeUpValue = (1 - score) * modFactor;
 		return score + (makeUpValue * score);
 	}
 }
+
+// public class Reasoner {
+// 	List<Action>     actions;
+// 	List<Mech>       enemies;
+// 	List<GameObject> others;
+// 	public Action decide() {
+// 		Action chosen = null;
+
+// 		foreach (Action action in actions) {
+// 			switch (action.actionType) {
+// 				case Idle: {
+// 					//
+// 				} break;
+
+// 				case Attack: {
+// 					//
+// 				} break;
+
+// 				case Hide: {
+// 					//
+// 				} break;
+
+// 				case Flee: {
+// 					//
+// 				} break;
+
+// 				case Shield: {
+// 					//
+// 				} break;
+
+// 				case EMP: {
+// 					//
+// 				} break;
+
+// 				case Charge: {
+// 					//
+// 				} break;
+
+// 				case Dodge: {
+// 					//
+// 				} break;
+
+// 				case Capture: {
+// 					//
+// 				} break;
+
+// 				case Defend: {
+// 					//
+// 				} break;
+
+// 				case Destroy: {
+// 					//
+// 				} break;
+
+// 				default: {
+// 					//
+// 				} break;
+// 			}
+// 		}
+// 	}
+// }
+
+// Action attack  = { actionType = attack  };
+// Action hide    = { actionType = hide    };
+// Action collect = { actionType = collect };
+// Action retreat = { actionType = retreat };
+
+// Reasoner rsn;
+// rsn.add( attack);
+// rsn.add(   hide);
+// rsn.add(collect);
+// rsn.add(retreat);
+
+// Action chosen = rsn.decide()
