7181f16886e74f96a7c6a5e5c64f04468ce803e8
SethSR <srenshaw1@gmail.com>
2015-10-21 19:49:30 -0500

    weapons deal with cooldown now

--- MechGame/Assets/Scripts/Weapon.cs
+++ MechGame/Assets/Scripts/Weapon.cs
@@ -2,6 +2,7 @@ using Vexe.Runtime.Types;
 using UnityEngine;
 
 public class Weapon : BetterBehaviour {
+	[HideInInspector] public Mech  owner;
 	[HideInInspector] public float fireTime;
 
 	public float cooldown;
@@ -10,11 +11,18 @@ public class Weapon : BetterBehaviour {
 	public float damage;
 
 	public float fire() {
-		fireTime = Time.time;
+		fireTime = cooldown;
 		return damage;
 	}
 
 	void Start() {
-		fireTime = Time.time;
+		fireTime = cooldown;
+	}
+
+	void Update() {
+		DebugExtension.DebugCircle(owner.transform.position, fireTime / cooldown);
+		if (fireTime > 0) {
+			fireTime -= Time.deltaTime;
+		}
 	}
 }
