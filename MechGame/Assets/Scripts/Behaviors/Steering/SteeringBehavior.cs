0d568c180549ff07c981f9c2bb80e96ffa5eedb4
SethSR <srenshaw1@gmail.com>
2015-10-25 03:19:01 -0500

    added debugging graphics to everything!!!

--- MechGame/Assets/Scripts/Behaviors/Steering/SteeringBehavior.cs
+++ MechGame/Assets/Scripts/Behaviors/Steering/SteeringBehavior.cs
@@ -15,10 +15,11 @@ static public class SteeringBehavior {
 			return Vector3.zero;
 		}
 		var average_heading = neighbors
-			.Select(m => m.transform.forward)
+			.Select(m => m.Heading)
 			.Aggregate((a,b) => a + b);
 		average_heading /= neighbors.Count;
-		average_heading -= vehicle.transform.forward;
+		average_heading -= vehicle.Heading;
+		DebugExtension.DebugArrow(vehicle.Position, average_heading, Color.green);
 		return average_heading;
 	}
 
@@ -26,17 +27,19 @@ static public class SteeringBehavior {
 		if (vehicle == null) {
 			return Vector3.zero;
 		}
-		var to_target = target - vehicle.transform.position;
+		var to_target = target - vehicle.Position;
 		var dist = to_target.magnitude;
 
+		var steering_force = Vector3.zero;
 		if (dist > 0) {
 			const float decel_tweaker = 1.2f;
 			var speed = dist / ((float)deceleration * decel_tweaker);
 			speed = Mathf.Min(speed, vehicle.maxSpeed);
 			var desired_velocity = to_target * speed / dist;
-			return desired_velocity - vehicle.velocity;
+			steering_force = desired_velocity - vehicle.Velocity;
 		}
-		return Vector3.zero;
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	static public Vector3 Cohesion(Mobile vehicle, ICollection<Mobile> neighbors) {
@@ -44,44 +47,55 @@ static public class SteeringBehavior {
 			return Vector3.zero;
 		}
 		var center_of_mass = neighbors
-			.Select(m => m.transform.position)
+			.Select(m => m.Position)
 			.Aggregate((a,b) => a + b);
 		center_of_mass /= neighbors.Count;
-		return Seek(vehicle, center_of_mass).normalized;
+		var steering_force = Seek(vehicle, center_of_mass).normalized;
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	static public Vector3 Evade(Mobile vehicle, Mobile pursuer, float threat_range = 100) {
 		if (vehicle == null || pursuer == null) {
 			return Vector3.zero;
 		}
-		var to_pursuer = pursuer.transform.position - vehicle.transform.position;
+		var to_pursuer = pursuer.Position - vehicle.Position;
 		if (to_pursuer.sqrMagnitude > threat_range * threat_range) {
 			return Vector3.zero;
 		}
-		var look_ahead_time = to_pursuer.magnitude / (vehicle.maxSpeed + pursuer.velocity.magnitude);
-		return Flee(vehicle, pursuer.transform.position + pursuer.velocity * look_ahead_time);
+		var look_ahead_time = to_pursuer.magnitude / (vehicle.maxSpeed + pursuer.Speed);
+		var steering_force = Flee(vehicle, pursuer.Position + pursuer.Velocity * look_ahead_time);
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		DebugExtension.DebugWireSphere(vehicle.Position, Color.red, threat_range);
+		return steering_force;
 	}
 
 	static public Vector3 Flee(Mobile vehicle, Vector3 target) {
 		if (vehicle == null) {
 			return Vector3.zero;
 		}
-		var desired_velocity = (vehicle.transform.position - target).normalized * vehicle.maxSpeed;
-		return desired_velocity - vehicle.velocity;
+		var desired_velocity = (vehicle.Position - target).normalized * vehicle.maxSpeed;
+		var steering_force = desired_velocity - vehicle.Velocity;
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	static public Vector3 FollowPath(Mobile vehicle, Path path, float waypointSeekDist) {
 		if (vehicle == null || path == null || path.IsEmpty) {
 			return Vector3.zero;
 		}
-		if ((path.CurrentWaypoint - vehicle.transform.position).sqrMagnitude < waypointSeekDist * waypointSeekDist) {
+		if ((path.CurrentWaypoint - vehicle.Position).sqrMagnitude < waypointSeekDist * waypointSeekDist) {
 			path.SetNextWaypoint();
 		}
+		var steering_force = Vector3.zero;
 		if (!path.Finished) {
-			return Seek(vehicle, path.CurrentWaypoint);
+			steering_force = Seek(vehicle, path.CurrentWaypoint);
 		} else {
-			return Arrive(vehicle, path.CurrentWaypoint);
+			steering_force = Arrive(vehicle, path.CurrentWaypoint);
 		}
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		DebugExtension.DebugWireSphere(vehicle.Position, Color.red, waypointSeekDist);
+		return steering_force;
 	}
 
 	static public Vector3 Hide(Mobile vehicle, Mobile hunter, ICollection<Transform> obstacles) {
@@ -94,35 +108,38 @@ static public class SteeringBehavior {
 		foreach (Transform cur_ob in obstacles) {
 			var hiding_spot = GetHidingPosition(cur_ob.position,
 			                                    cur_ob.GetComponent<SphereCollider>().radius,
-			                                    hunter.transform.position);
-			DebugExtension.DebugPoint(hiding_spot);
+			                                    hunter.Position);
+			DebugExtension.DebugPoint(hiding_spot, Color.black);
 
-			var dist = (hiding_spot - vehicle.transform.position).magnitude;
+			var dist = (hiding_spot - vehicle.Position).magnitude;
 			if (dist < dist_to_closest) {
 				dist_to_closest  = dist;
 				best_hiding_spot = hiding_spot;
 			}
 		}
-
+		var steering_force = Vector3.zero;
 		if (dist_to_closest == float.MaxValue) {
-			return Evade(vehicle, hunter);
+			steering_force = Evade(vehicle, hunter);
 		} else {
-			DebugExtension.DebugArrow(vehicle.transform.position, best_hiding_spot - vehicle.transform.position, Color.green);
-			return Arrive(vehicle, best_hiding_spot, Deceleration.fast);
+			steering_force = Arrive(vehicle, best_hiding_spot, Deceleration.fast);
 		}
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	static public Vector3 Interpose(Mobile vehicle, Mobile agent_a, Mobile agent_b) {
 		if (vehicle == null || agent_a == null || agent_b == null) {
 			return Vector3.zero;
 		}
-		var mid_point = (agent_a.transform.position + agent_b.transform.position) / 2;
-		var time_to_reach_mid_point = (vehicle.transform.position - mid_point).magnitude / vehicle.maxSpeed;
-		var pos_a = agent_a.transform.position + agent_a.velocity * time_to_reach_mid_point;
-		var pos_b = agent_b.transform.position + agent_b.velocity * time_to_reach_mid_point;
+		var mid_point = (agent_a.Position + agent_b.Position) / 2;
+		var time_to_reach_mid_point = (vehicle.Position - mid_point).magnitude / vehicle.maxSpeed;
+		var pos_a = agent_a.Position + agent_a.Velocity * time_to_reach_mid_point;
+		var pos_b = agent_b.Position + agent_b.Velocity * time_to_reach_mid_point;
 		mid_point = (pos_a + pos_b) / 2;
-		DebugExtension.DebugArrow(vehicle.transform.position, mid_point - vehicle.transform.position, Color.green);
-		return Arrive(vehicle, mid_point, Deceleration.fast);
+		DebugExtension.DebugPoint(mid_point, Color.green);
+		var steering_force = Arrive(vehicle, mid_point, Deceleration.fast);
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	//FIXME(seth): Fix this!! It's doing something weird
@@ -130,8 +147,9 @@ static public class SteeringBehavior {
 		if (vehicle == null || obstacles.Count == 0) {
 			return Vector3.zero;
 		}
-		//the detection box length is proportional to the agent's velocity
-		var detect_box_length = min_detection_box_length + (vehicle.velocity.magnitude / vehicle.maxSpeed) * min_detection_box_length;
+		//the detection box length is proportional to the agent's Velocity
+		var detect_box_length = min_detection_box_length + (vehicle.Speed / vehicle.maxSpeed) * min_detection_box_length;
+		DebugExtension.DebugCylinder(vehicle.Position, vehicle.Position + vehicle.Heading * detect_box_length, Color.red, vehicle.GetComponent<SphereCollider>().radius);
 
 		//this will keep track of the closest intersecting obstacle (CIB)
 		Transform closest_intersecting_obstacle = null;
@@ -144,7 +162,7 @@ static public class SteeringBehavior {
 
 		foreach (var cur_ob in obstacles) {
 			//calculate this obstacle's position in local space
-			var local_pos = vehicle.transform.position + Quaternion.LookRotation(vehicle.transform.forward) * cur_ob.position;
+			var local_pos = Quaternion.FromToRotation(vehicle.Heading, Vector3.forward) * (cur_ob.position - vehicle.Position);
 
 			//if the local position has a negative x value then it must lay
 			//behind the agent. (in which case it can be ignored)
@@ -203,33 +221,40 @@ static public class SteeringBehavior {
 		}
 
 		//finally, convert the steering vector from local to world space
-		var force = vehicle.transform.position + Quaternion.LookRotation(Vector3.forward - vehicle.transform.forward) * steering_force;
-		DebugExtension.DebugArrow(vehicle.transform.position, force - vehicle.transform.position, Color.black);
-		return force;
+		var force = vehicle.Position + Quaternion.LookRotation(Vector3.forward - vehicle.Heading) * steering_force;
+		DebugExtension.DebugArrow(vehicle.Position, force - vehicle.Position, Color.green);
+		// return force;
+		return Vector3.zero;
 	}
 
 	static public Vector3 OffsetPursuit(Mobile vehicle, Mobile leader, Vector3 offset) {
 		if (vehicle == null || leader == null) {
 			return Vector3.zero;
 		}
-		var world_offset_pos = leader.transform.position + Quaternion.LookRotation(leader.transform.forward) * offset;
-		var to_offset = world_offset_pos - vehicle.transform.position;
-		var look_ahead_time = to_offset.magnitude / (vehicle.maxSpeed + leader.velocity.magnitude);
-		return Arrive(vehicle, world_offset_pos + leader.velocity * look_ahead_time, Deceleration.fast);
+		var world_offset_pos = leader.Position + Quaternion.LookRotation(leader.Heading) * offset;
+		var to_offset = world_offset_pos - vehicle.Position;
+		var look_ahead_time = to_offset.magnitude / (vehicle.maxSpeed + leader.Speed);
+		var steering_force = Arrive(vehicle, world_offset_pos + leader.Velocity * look_ahead_time, Deceleration.fast);
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	static public Vector3 Pursuit(Mobile vehicle, Mobile evader) {
 		if (vehicle == null || evader == null) {
 			return Vector3.zero;
 		}
-		var to_evader = evader.transform.position - vehicle.transform.position;
-		var relative_heading = Vector3.Dot(vehicle.transform.forward, evader.transform.forward);
-		if (Vector3.Dot(to_evader, vehicle.transform.forward) > 0 && relative_heading < -0.95f) {
-			return Seek(vehicle, evader.transform.position);
+		var to_evader = evader.Position - vehicle.Position;
+		var relative_heading = Vector3.Dot(vehicle.Heading, evader.Heading);
+
+		Vector3 steering_force = Vector3.zero;
+		if (Vector3.Dot(to_evader, vehicle.Heading) > 0 && relative_heading < -0.95f) {
+			steering_force = Seek(vehicle, evader.Position);
 		} else {
-			var look_ahead_time = to_evader.magnitude / (vehicle.maxSpeed + evader.velocity.magnitude);
-			return Seek(vehicle, evader.transform.position + evader.velocity * look_ahead_time);
+			var look_ahead_time = to_evader.magnitude / (vehicle.maxSpeed + evader.Speed);
+			steering_force = Seek(vehicle, evader.Position + evader.Velocity * look_ahead_time);
 		}
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	static public Vector3 Separation(Mobile vehicle, ICollection<Mobile> neighbors) {
@@ -237,64 +262,35 @@ static public class SteeringBehavior {
 			return Vector3.zero;
 		}
 		var steering_force = neighbors
-			.Select(m => vehicle.transform.position - m.transform.position)
+			.Select(m => vehicle.Position - m.Position)
 			.Select(v => v.normalized / v.magnitude)
 			.Aggregate((a,b) => a + b);
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
 		return steering_force;
 	}
 
-	//FIXME(seth): Not sure if this works currently
 	static public Vector3 WallAvoidance(Mobile vehicle, ICollection<Transform> walls, float wall_feeler_length) {
 		if (vehicle == null || walls.Count == 0) {
 			return Vector3.zero;
 		}
-		//the feelers are contained in a std::vector, m_Feelers
-		List<Vector3> feelers = CreateFeelers(wall_feeler_length);
-
-		var dist_to_this_ip    = 0.0f;
-		var dist_to_closest_ip = float.MaxValue;
-
-		//this will hold an index into the vector of walls
-		Transform closest_wall = null;
-
-		var steering_force = Vector3.zero;
-		var closest_point = Vector3.zero;  //holds the closest intersection point
-		var closest_normal = Vector3.zero;
-
-		//examine each feeler in turn
+		List<Vector3> feelers          = CreateFeelers(wall_feeler_length);
+		Vector3       steering_force   = Vector3.zero;
+		Vector3       closest_point    = Vector3.zero;
+		float         closest_distance = float.MaxValue;
 		foreach (var feeler in feelers) {
-			var rot_feeler = Quaternion.LookRotation(vehicle.transform.forward) * feeler;
-			DebugExtension.DebugArrow(vehicle.transform.position, rot_feeler);
-			//run through each wall checking for any intersection points
-			foreach (var wall in walls) {
-				RaycastHit hit_info;
-				if (Physics.Linecast(vehicle.transform.position, rot_feeler + vehicle.transform.position, out hit_info)) {
-					DebugExtension.DebugPoint(hit_info.point, Color.red);
-					dist_to_this_ip = hit_info.distance;
-					//is this the closest found so far? If so keep a record
-					if (dist_to_this_ip < dist_to_closest_ip) {
-						dist_to_closest_ip = dist_to_this_ip;
-						closest_wall = wall;
-						closest_point = hit_info.point;
-						closest_normal = hit_info.normal;
-					}
+			var rot_feeler = vehicle.transform.TransformVector(feeler);
+			DebugExtension.DebugArrow(vehicle.Position, rot_feeler, Color.gray);
+			RaycastHit hit_info;
+			if (Physics.Linecast(vehicle.Position, rot_feeler + vehicle.Position, out hit_info)) {
+				DebugExtension.DebugPoint(hit_info.point, Color.red);
+				if (hit_info.distance < closest_distance) {
+					closest_distance = hit_info.distance;
+					closest_point    = hit_info.point;
+					steering_force   = hit_info.normal * (rot_feeler - closest_point).magnitude;
 				}
-			}//next wall
-
-
-			//if an intersection point has been detected, calculate a force  
-			//that will direct the agent away
-			if (closest_wall != null) {
-				//calculate by what distance the projected position of the agent
-				//will overshoot the wall
-				var overshoot = rot_feeler - closest_point;
-
-				//create a force in the direction of the wall normal, with a 
-				//magnitude of the overshoot
-				steering_force = closest_normal * overshoot.magnitude;
 			}
-		}//next feeler
-
+		}
+		DebugExtension.DebugArrow(closest_point, steering_force, Color.green);
 		return steering_force;
 	}
 
@@ -308,24 +304,26 @@ static public class SteeringBehavior {
 		                             (Random.value * 2 - 1) * jitter_this_time_slice);
 		wander_target.Normalize();
 		wander_target *= radius;
-		return wander_target + vehicle.transform.forward * distance;
+		var steering_force = wander_target + vehicle.Heading * distance;
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 	static public Vector3 Seek(Mobile vehicle, Vector3 target) {
 		if (vehicle == null) {
 			return Vector3.zero;
 		}
-		var desired_velocity = (target - vehicle.transform.position).normalized * vehicle.maxSpeed;
-		return desired_velocity - vehicle.velocity;
+		var desired_velocity = (target - vehicle.Position).normalized * vehicle.maxSpeed;
+		var steering_force = desired_velocity - vehicle.Velocity;
+		DebugExtension.DebugArrow(vehicle.Position, steering_force, Color.green);
+		return steering_force;
 	}
 
 
 	static Vector3 GetHidingPosition(Vector3 pos_ob, float radius_ob, Vector3 pos_hunter) {
 		const float distance_from_boundary = 3f;
 		var dist_away = radius_ob + distance_from_boundary;
-
 		var to_ob = (pos_ob - pos_hunter).normalized;
-
 		return (to_ob * dist_away) + pos_ob;
 	}
 
