4fb7fc7741d9b8a4074557193b16194bcab5efe9
SethSR <srenshaw1@gmail.com>
2015-11-08 03:11:17 -0600

    Yay test stuff!

---
+++ MechGame/Assets/Scripts/TestStuff/ObstacleSpawner.cs
@@ -0,0 +1,24 @@
+﻿using Vexe.Runtime.Types;
+using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawner : BetterBehaviour {
+	[iMin( 1)] public int   numberOfObstacles;
+	[fMin(10)] public float spawnRadius;
+	[fMin( 1)] public float obstacleSizeMin;
+	[fMin( 1)] public float obstacleSizeMax;
+
+	void Start() {
+		for (int i = numberOfObstacles; i --> 0;) {
+			GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			go.transform.position = Random.insideUnitSphere * spawnRadius;
+			go.transform.localScale = Vector3.one * (obstacleSizeMin + Random.value * (obstacleSizeMax - obstacleSizeMin));
+			go.transform.parent = transform;
+			go.tag = "Obstacle";
+		}
+	}
+
+	void Update() {
+		DebugExtension.DebugWireSphere(transform.position, Color.gray, spawnRadius);
+	}
+}
---
+++ MechGame/Assets/Scripts/TestStuff/ResourceSpawner.cs
@@ -0,0 +1,26 @@
+﻿using Vexe.Runtime.Types;
+using UnityEngine;
+using System.Collections;
+
+public class ResourceSpawner : MonoBehaviour {
+	public Resource prefab;
+	[iMin( 1)] public int numberOfResourceNodes;
+	[fMin(10)] public float spawnRadius;
+	[fMin( 1)] public float resourceAmountMin;
+	[fMin( 1)] public float resourceAmountMax;
+
+	void Start() {
+		for (int i = numberOfResourceNodes; i --> 0;) {
+			var position = Random.insideUnitSphere * spawnRadius;
+			Resource res = (Resource)Instantiate(prefab, position, Quaternion.identity);
+			res.startingAmount = resourceAmountMin + Random.value * (resourceAmountMax - resourceAmountMin);
+			res.currentAmount = res.startingAmount;
+			res.transform.parent = transform;
+			res.tag = "Resource";
+		}
+	}
+	
+	void Update() {
+		DebugExtension.DebugWireSphere(transform.position, Color.gray, spawnRadius);
+	}
+}
