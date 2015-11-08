815e824f91fbe365e496571e881f648332657956
SethSR <srenshaw1@gmail.com>
2015-11-08 03:07:05 -0600

    Added a main container for entities
    Entities shouldn't need their own collection of other entities in the
    scene.

---
+++ MechGame/Assets/Scripts/EntityContainer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EntityContainer : MonoBehaviour {
+	public List<Mech>      mechs;
+	public List<Resource>  resources;
+	public List<Transform> obstacles;
+
+	void Awake() {
+		var mech_transform = transform.Find("Mechs");
+		if (mech_transform != null) {
+			foreach (Mech mech in mech_transform) {
+				mechs.Add(mech);
+			}
+		}
+		var res_transform = transform.Find("Resources");
+		if (res_transform != null) {
+			foreach (Resource res in res_transform) {
+				resources.Add(res);
+			}
+		}
+		var obs_transform = transform.Find("Obstacles");
+		if (obs_transform != null) {
+			foreach (Transform obs in obs_transform) {
+				obstacles.Add(obs);
+			}
+		}
+	}
+}
