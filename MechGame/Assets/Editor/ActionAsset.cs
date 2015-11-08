3507b9765f91651fcb225fbfb613cd5a1f9c3c8b
SethSR <srenshaw1@gmail.com>
2015-11-08 03:02:24 -0600

    Menu option for creating Action scriptable objects

---
+++ MechGame/Assets/Editor/ActionAsset.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ActionAsset {
+	[MenuItem("Assets/Create/Action")]
+	public static void CreateAsset() {
+		ScriptableObjectUtility.CreateAsset<Action>();
+	}
+}
