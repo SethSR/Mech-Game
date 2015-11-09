6679bbf542480a292551b02cc2fb293aa511a8ed
SethSR <srenshaw1@gmail.com>
2015-11-08 20:27:44 -0600

    Trimmed a lot of "fat".
    Trying to make my components more modular, and less dependent.

--- MechGame/Assets/Scripts/Mech.cs
+++ MechGame/Assets/Scripts/Mech.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 
-[RequireComponent(typeof(Mobile))]
 public class Mech : BetterBehaviour {
 	[HideInInspector] public Transform target;
 	[HideInInspector] public float     currentHealth;
@@ -15,52 +14,26 @@ public class Mech : BetterBehaviour {
 	[fMin(1)] public float sensorRange      = 100; // in meters
 	          public int   maxTeamSize      =   5; //TODO(seth): move this to the mechs carrier/commander
 
-	public Weapon mainWep;
-	public Weapon sideWep;
-	public Weapon backWep;
-
-	public void fireWeaponAt(Mech enemy) {
-		if (CurrentWeapon.fireTime < 0) {
-			DebugExtension.DebugArrow(transform.position, enemy.transform.position - transform.position, Color.red);
-			enemy.currentHealth -= CurrentWeapon.fire();
-		}
+	public void ActivateLeftArm() {
+		Debug.Log(name + " activate left arm equipment");
 	}
 
-	[HideInInspector] public Weapon CurrentWeapon {
-		get {
-			//TODO(seth): Choose the most useful weapon somehow
-			// for now just pick the main weapon
-			return mainWep;
-		}
+	public void ActivateRightArm() {
+		Debug.Log(name + " activate right arm equipment");
 	}
 
 
 	void Start() {
 		currentHealth = totalHealth;
-		mainWep = Instantiate<Weapon>(mainWep); mainWep.owner = this;
-		sideWep = Instantiate<Weapon>(sideWep); sideWep.owner = this;
-		backWep = Instantiate<Weapon>(backWep); backWep.owner = this;
 	}
 
 	Color sensorRangeColor = new Color(0, 1, 0, 0.75f);
 
 	void Update() {
 		DebugExtension.DebugWireSphere(transform.position, sensorRangeColor, sensorRange);
-		if (playerControlled) {
-			// do stuff
-		} else {
-			GetComponent<Reasoner>().DecideOnAction();
-		}
-
 		if (currentHealth < 0) {
 			// has died
 			Destroy(gameObject);
 		}
-	}
-
-	void OnDestroy() {
-		if (mainWep != null) { Destroy(mainWep.gameObject); }
-		if (sideWep != null) { Destroy(sideWep.gameObject); }
-		if (backWep != null) { Destroy(backWep.gameObject); }
 	}
 }
