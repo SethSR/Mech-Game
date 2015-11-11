199d5a877df3fb5b47c5aac0118375748acfe092
SethSR <srenshaw1@gmail.com>
2015-11-10 22:08:48 -0600

    The camera now shakes when moving quickly

---
+++ MechGame/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+	public float shake;
+
+	void Update() {
+		var speed = transform.parent.GetComponent<Rigidbody>().velocity.magnitude;
+		if (speed > 0) {
+			transform.position = transform.parent.position + transform.parent.rotation * (Vector3.up + Random.insideUnitSphere * Mathf.Lerp(shake, 0, 10/speed));
+		}
+	}
+}
